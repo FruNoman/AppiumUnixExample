c38ad5b4bb980def84ca96e3d25c2d6a9d321e74
Dmytro A Frolov <dmytro.a.frolov@globallogic.com>
2019-01-11 15:01:28 +0200

    simple test

--- src/test/java/com/github/frunoman/pages/BasePage.java
+++ src/test/java/com/github/frunoman/pages/BasePage.java
@@ -5,16 +5,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import java.util.List;
 
-public class BasePage {
-    private WebDriver driver;
+public abstract class BasePage {
+    protected WebDriver driver;
+
 
     public BasePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
-
     }
 
 
--- src/test/java/com/github/frunoman/pages/MainPage.java
+++ src/test/java/com/github/frunoman/pages/MainPage.java
@@ -1,29 +1,28 @@
 package com.github.frunoman.pages;
 
 import com.github.frunoman.utils.Categories;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 
 import java.util.List;
 
 public class MainPage extends BasePage{
-
-    private WebDriver driver;
+    @AndroidFindBy(id = "")
     @FindBy(css = "ul.menu-categories>li>a")
     private List<WebElement> mainCategories;
 
     public MainPage(WebDriver driver) {
         super(driver);
-        this.driver = driver;
     }
 
 
     @Step("Щелкнуть по категории")
     public void clickOnCategory(Categories category) {
-//        waitForElements(mainCategories);
         for(WebElement element:mainCategories){
             if(element.getText().equals(category.toString())){
                 element.click();
